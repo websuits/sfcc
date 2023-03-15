0601bfea7a0c41a379786687b0e6484e14bf18b2
Stefan Luntraru <stefanluntraru@Stefans-MacBook-Air-2.local>
2023-03-15 22:30:02 +0200

    ex2: exercise 2 toggle coupon

--- cartridges/app_storefront_sluntraru/cartridge/client/default/js/custom/toggle.js
+++ cartridges/app_storefront_sluntraru/cartridge/client/default/js/custom/toggle.js
@@ -1,5 +1,5 @@
 window.jQuery = window.$ = require('jquery');
-$('.form-toggle-anchor').css('color', 'red');
+$('.form-toggle-anchor').css("background-color", "red");
 $('.form-toggle-anchor').click(function (e) {
     e.preventDefault();
     $('.promo-code-form').toggle();
