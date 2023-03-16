5d9c1b1c6199babd844b826dd52b0666dfe70565
Stefan Luntraru <stefanluntraru@Stefans-MacBook-Air-2.local>
2023-03-16 02:22:30 +0200

    ex2: fixed webpack, added css version

--- cartridges/app_storefront_sluntraru/cartridge/client/default/js/custom/toggle.js
+++ cartridges/app_storefront_sluntraru/cartridge/client/default/js/custom/toggle.js
@@ -1,7 +1,14 @@
 window.jQuery = window.$ = require('jquery');
-$('.form-toggle-anchor').css("background-color", "red");
+
+// $('.form-toggle-anchor').click(function (e) {
+//     e.preventDefault();
+//     $('.form-toggle-anchor').hide()
+//     $('.promo-code-input').show();
+// });
+
+
 $('.form-toggle-anchor').click(function (e) {
     e.preventDefault();
-    $('.form-toggle-anchor').hide()
-    $('.promo-code-input').show();
+    $('.form-toggle-anchor').addClass('hidden');
+    $('.promo-code-input').toggleClass( 'hidden' );
 });
